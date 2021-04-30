aca30e54793696f2a86c736db2d394862c33b714
Moonika1991 <monika9107@gmail.com>
2021-04-30 11:20:32 +0200

    Line chart can be now created for one line or multiple lines

--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -6,21 +6,30 @@ import moment from 'moment';
 export default class LineChart extends React.Component {
   constructor() {
 		super();
+    this.generateData = this.generateData.bind(this);
 		this.generateDataPoints = this.generateDataPoints.bind(this);
 	}
 
-  generateDataPoints(data) {
+  generateData(inData) {
+    var data = []
+    const dictList = JSON.parse(inData);
+    for (const [key, value] of Object.entries(dictList)) {
+      const dataPoints = this.generateDataPoints(value);
+      console.log(dataPoints);
+      data.push({ type: 'line', name: dataPoints[0], showInLegend: true, dataPoints: dataPoints[1] });
+    }
+    return data;
+  }
+
+  generateDataPoints(dict) {
       var dps = [];
-      const dictList = JSON.parse(data);
-      const dict = dictList[0];
-      const titleKey = Object.keys(dict)[0];
       let title = "";
+      const titleKey = Object.keys(dict)[0];
       if(typeof(dict[titleKey]) === 'string'){
         title = dict[titleKey];
         delete dict[titleKey];
       }
       var xVal;
-      var flag = 0;
       for (const [key, value] of Object.entries(dict)){
           if (moment(key).isValid()){
               xVal = new Date(key);
@@ -33,19 +42,12 @@ export default class LineChart extends React.Component {
   }
 
   render() {
-    const data = this.generateDataPoints(this.props.dataToDisplay);
+    const data = this.generateData(this.props.dataToDisplay);
     const options = {
 			theme: "light2", // "light1", "dark1", "dark2"
 			animationEnabled: true,
 			zoomEnabled: true,
-      title:{
-        text: data[0]
-      },
-			data: [{
-				type: "line",
-        xValueFormatString: "DD-MM-YY",
-				dataPoints: data[1]
-			}]
+			data: data
 		};
 
     return (
