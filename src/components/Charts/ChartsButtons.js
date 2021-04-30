008a07b8520adaf6f97b9b07ad78f489310c1ef1
Moonika1991 <monika9107@gmail.com>
2021-04-30 10:19:05 +0200

    Pie chart added

--- src/components/Charts/ChartsButtons.js
+++ src/components/Charts/ChartsButtons.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap'
-import { AiOutlineLineChart, AiOutlineBarChart, AiOutlinePieChart, AiOutlineAreaChart } from 'react-icons/ai'
+import { AiOutlineLineChart, AiOutlineBarChart, AiOutlinePieChart } from 'react-icons/ai'
 import LineChart from '../Charts/LineChart'
 import ColumnChart from '../Charts/ColumnChart'
+import PieChart from '../Charts/PieChart'
 
 
 export default class ChartsButtons extends React.Component {
@@ -11,6 +12,7 @@ export default class ChartsButtons extends React.Component {
     this.state = {
       showHideLineChart: false,
       showHideColumnChart: false,
+      showHidePieChart: false,
     };
     this.hideComponent = this.hideComponent.bind(this);
   }
@@ -23,26 +25,31 @@ export default class ChartsButtons extends React.Component {
           break;
         case "showHideColumnChart":
           this.setState({ showHideColumnChart: !this.state.showHideColumnChart });
+          break;
+        case "showHidePieChart":
+          this.setState({ showHidePieChart: !this.state.showHidePieChart });
+          break;
       }
   }
 
   render() {
     const { showHideLineChart } = this.state;
     const { showHideColumnChart } = this.state;
+    const { showHidePieChart } = this.state;
     return (
       <Container>
         <Row className="justify-content-md-center">
           <Col>
             <Button variant= "info" className="m-3 shadow" size="lg"><AiOutlineLineChart size={50} onClick={() => this.hideComponent("showHideLineChart")} /></Button>
-            <Button variant= "info" className="m-3 shadow" size="lg"><AiOutlineBarChart size={50} onClick={() => this.hideComponent("showHideColumnChart")}/></Button>
-            <Button variant= "info" className="m-3 shadow" size="lg"><AiOutlinePieChart size={50} /></Button>
-            <Button variant= "info" className="m-3 shadow" size="lg"><AiOutlineAreaChart size={50} /></Button>
+            <Button variant= "info" className="m-3 shadow" size="lg"><AiOutlineBarChart size={50} onClick={() => this.hideComponent("showHideColumnChart")} /></Button>
+            <Button variant= "info" className="m-3 shadow" size="lg"><AiOutlinePieChart size={50} onClick={() => this.hideComponent("showHidePieChart")} /></Button>
           </Col>
         </Row>
         <Row>
           <Col className="justify-content-md-center">
               {showHideLineChart && <LineChart dataToDisplay = {this.props.dataFromParent} />}
               {showHideColumnChart && <ColumnChart dataToDisplay = {this.props.dataFromParent} />}
+              {showHidePieChart && <PieChart dataToDisplay = {this.props.dataFromParent} />}
           </Col>
         </Row>
       </Container>
